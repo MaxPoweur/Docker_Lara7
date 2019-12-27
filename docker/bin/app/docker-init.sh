e04b4560bb7c9fb282356653b6618666eae16e1a
Adam Rotard <adamrotard@gmail.com>
2019-12-27 14:35:13 +0100

    Publish env vars into Apache/PHP

--- docker/bin/app/docker-init.sh
+++ docker/bin/app/docker-init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo 'WebApp container for LARA7 started'
+
+# Import our environment variables into Apache/PHP
+sudo sed -i '$a export DB_CONNECTION='"$DB_CONNECTION" /etc/apache2/envvars
+sudo sed -i '$a export DB_HOST='"$DB_HOST" /etc/apache2/envvars
+sudo sed -i '$a export DB_PORT='"$DB_PORT" /etc/apache2/envvars
+sudo sed -i '$a export DB_USERNAME='"$DB_USERNAME" /etc/apache2/envvars
+sudo sed -i '$a export DB_PASSWORD='"$DB_PASSWORD" /etc/apache2/envvars
+
 sudo /usr/sbin/apache2ctl -D FOREGROUND # Starts apache service
